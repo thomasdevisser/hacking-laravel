50b6e9c981f5cb148cfa38017a20651f4cf4ec92
Thomas de Visser <118618474+thomasdevisser@users.noreply.github.com>
2022-12-16 19:02:44 +0100

    Added post migration, model and creation route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,6 +12,24 @@ class PostController extends Controller
     }
 
     public function create(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => [
+                'required',
+                'min:4',
+                'max:50',
+            ],
+            'body' => [
+                'required',
+                'min:4',
+            ]
+        ]);
 
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
+
+        return redirect('/')->with('success', 'Post created successfully!');
     }
 }
