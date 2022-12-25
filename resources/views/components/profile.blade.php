e9b4c3d684747cb6c3a6f8139b9f566058be2b97
Thomas de Visser <118618474+thomasdevisser@users.noreply.github.com>
2022-12-25 12:14:42 +0100

    Fix error on profile page for guests

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -5,13 +5,13 @@
   <div class="profile-header">
     <img src="{{ $avatar }}" alt="Profile Image">
     <h1>{{ $username }}</h1>
-    @if(auth()->user()->username != $username)
+    @if(auth()?->user()?->username != $username)
       <form action="/{{ $isFollowing ? 'unfollow' : 'follow' }}/{{ $username }}" method="POST">
         @csrf
         <button>{{ $isFollowing ? 'Unfollow' : 'Follow' }}</button>
       </form>
     @endif
-    @if(auth()->user()->username == $username)
+    @if(auth()?->user()?->username == $username)
       <a href="/update-profile-image">Update Profile Image</a>
     @endif
   </div>
